63171d46b4709a335fbfe022196185ff1e9c646c
杨玉东 <yyd@duiba.com.cn>
2023-07-09 09:10:18 +0800

    Initial commit

---
+++ src/main/java/com/duiba/student/DuibaStudentApplication.java
@@ -0,0 +1,13 @@
+package com.duiba.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuibaStudentApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run( DuibaStudentApplication.class, args );
+      }
+
+}
---
+++ src/test/java/com/duiba/student/DuibaStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duiba.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuibaStudentApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
